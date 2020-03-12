841b255e549d658c0a51425ac5b01feb858473f2
Nickolazz1 <elanir358@gmail.com>
2020-03-12 16:04:07 +0200

    first run

--- Sample-Test1/pch.cpp
+++ Sample-Test1/pch.cpp
@@ -4,3 +4,13 @@
 //
 
 #include "pch.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello google test" << endl;
+
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
