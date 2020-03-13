7d6f72e35a6ba8caacbcbd434010a57110f0e549
Nickolazz1 <elanir358@gmail.com>
2020-03-13 15:14:49 +0200

    added some more tests

--- Sample-Test1/pch.h
+++ Sample-Test1/pch.h
@@ -8,3 +8,34 @@
 #include "gtest/gtest.h"
 
 #include <iostream>
+
+struct BankAccount
+{
+	int balance = 0;
+
+	BankAccount()
+	{
+
+	}
+
+	explicit BankAccount(const int balance)
+		:balance{ balance }
+	{
+
+	}
+};
+
+struct BankAccountTest : testing::Test
+{
+	BankAccount* account;
+
+	BankAccountTest()
+	{
+		account = new BankAccount;
+	}
+
+	~BankAccountTest()
+	{
+		delete account;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName_first, TestName_AssertionsReview) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(AccountTest, BankAccountStartsEmpty) {
+    BankAccount account;
+    EXPECT_EQ(0, account.balance);
+}
+
+TEST_F(BankAccountTest, BankAccountStartsEmpty) {
+    EXPECT_EQ(0, account->balance);
 }
