e9724a4aca9be97b6ce04e3df7cc0dd0379d566e
Nickolazz1 <elanir358@gmail.com>
2020-03-14 13:59:40 +0200

    assertions fatal and non-fatal

--- Sample-Test1/pch.cpp
+++ Sample-Test1/pch.cpp
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+TEST( CodesBay1, Subtest1) {
+	ASSERT_EQ(1, 2);
+	cout << "After assertion 1" << endl;
+}
+
+TEST(CodesBay2, Subtest2) {
+	EXPECT_EQ(1, 2);
+	cout << "After assertion 2" << endl;
+}
+
 int main(int argc, char* argv[])
 {
 	cout << "Hello google test" << endl;
